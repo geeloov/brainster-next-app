a0bff37f825a5ae4be1885ee661595c39797164a
deko96 <bozhinoski@icloud.com>
2023-12-18 23:17:22 +0100

    Install jest + ts + eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+/* eslint-env node */
+module.exports = {
+  env: {
+    "jest/globals": true,
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "jest"],
+  root: true,
+  rules: {
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/explicit-function-return-type": "error",
+  },
+};
