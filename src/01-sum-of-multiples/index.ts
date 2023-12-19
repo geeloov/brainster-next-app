95da0da731a8fde497054543d866ca786633ff49
deko96 <bozhinoski@icloud.com>
2023-12-19 10:34:35 +0100

    Refactor exercises to use README.md for better readability

--- src/01-sum-of-multiples/index.ts
+++ src/01-sum-of-multiples/index.ts
@@ -1,13 +1,1 @@
-/**
- * Exercise 01: Sum of multiples
- *
- * Write a TypeScript function sumOfMultiples that calculates the sum of all multiples
- * of a given number up to a specified maximum value.
- * The function should be typesafe, and it should specify the return type
- *
- * Example:
- * sumOfMultiples(3, 10) => 3 + 6 + 9 = 18
- * sumOfMultiples(4, 3) => 0 (no multiples because multiple is greater than 3)
- */
-
 export function sumOfMultiples(multiple, max) {}
--- src/03-email-extractor/index.ts
+++ src/03-email-extractor/index.ts
@@ -1,19 +1,1 @@
-/**
- * Exercise 02: Email extractor
- *
- *
- * The problem we are trying to solve with the extractEmails function
- * is to extract a specific property value from a list of objects.
- * In this case, the function should be designed to take an array of user objects and extract their email addresses.
- *
- * Example:
- * const users = [
- *   { id: 1, name: "Alice", email: "alice@example.com", age: 30 },
- *   { id: 2, name: "Bob", email: "bob@example.com", age: 25 },
- *   { id: 3, name: "Charlie", email: "charlie@example.com", age: 35 },
- * ];
- *
- * extractEmails(users) => ["alice@example.com", "bob@example.com", "charlie@example.com"]
- */
-
 export function extractEmails(users) {}
