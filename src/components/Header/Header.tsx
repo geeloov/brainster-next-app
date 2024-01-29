fc2b26f308211247bf9014f282440cfb8e8198a7
deko96 <bozhinoski@icloud.com>
2024-01-29 22:02:50 +0000

    Render Header as client component + layout SSR

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,9 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import DarkModeSwitcher from "../DarkModeSwitcher/DarkModeSwitcher";
 
-interface HeaderProps {}
-
-const Header = (props: HeaderProps) => {
+const Header = () => {
   return (
     <header className="sticky top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
       <div className="flex flex-grow items-center justify-between px-4 py-4 shadow-2 md:px-6 2xl:px-11">
